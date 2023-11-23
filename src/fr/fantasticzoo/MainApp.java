b9daa8746acd7b022ff533cd86944dcdd28fae3c
ClementMARIEM22004869 <clement.marie24@gmail.com>
2023-11-23 10:54:29 +0100

    update die system

--- src/fr/fantasticzoo/MainApp.java
+++ src/fr/fantasticzoo/MainApp.java
@@ -6,7 +6,7 @@ import fr.fantasticzoo.view.GameEngine;
 public class MainApp {
 
     public static void main(String[] args) {
-        System.out.println("Binvenue au Zootopie ! \n");
+        System.out.println("Welcome to Zootopie ! \n");
 
         GameEngine gameEngine = new GameEngine();
         gameEngine.startGame();
